354842b7e46c38952d4f10bb3109e7f5684d8921
DikaArieArrifky <20dika16@gmail.com>
2024-03-03 19:13:05 +0700

    09_Percobaan3_pertanyaan3

--- Minggu3/ArraySegitiga/Segitiga.java
+++ Minggu3/ArraySegitiga/Segitiga.java
@@ -1,4 +1,5 @@
 package Minggu3.ArraySegitiga;
+import java.lang.Math;
 
 public class Segitiga {
     public int alas;
@@ -8,5 +9,13 @@ public class Segitiga {
         alas = a;
         tinggi = t;
     }
+
+    public double hitungVolume(){
+        return  0.5 * alas *tinggi;
+    }
     
+    public double hitungKeliling(){
+        double sisiMiring = Math.sqrt(Math.pow(alas, 2)+ Math.pow(tinggi, 2));
+        return alas + tinggi + sisiMiring;
+    }
 }

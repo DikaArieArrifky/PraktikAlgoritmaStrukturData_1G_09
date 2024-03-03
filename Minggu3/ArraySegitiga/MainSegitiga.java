b0124ddfa927b92cd290f9ba74e92ff6d4e69062
DikaArieArrifky <20dika16@gmail.com>
2024-03-03 19:18:01 +0700

    09_Percobaan3_Pertanyaan4

---
+++ Minggu3/ArraySegitiga/MainSegitiga.java
@@ -0,0 +1,14 @@
+package Minggu3.ArraySegitiga;
+
+public class MainSegitiga {
+    public static void main(String[] args) {
+        
+        Segitiga [] sgArray09 = new Segitiga[4];
+
+        sgArray09[0] = new Segitiga(10, 4);
+        sgArray09[1] = new Segitiga(20, 10);
+        sgArray09[2] = new Segitiga(15, 6);
+        sgArray09[3] = new Segitiga(25, 10);
+
+    }
+}

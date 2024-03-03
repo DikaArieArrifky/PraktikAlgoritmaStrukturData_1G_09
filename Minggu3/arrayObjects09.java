5a0f0ba559ff4fb1d6c5868602140824c7c5dca6
DikaArieArrifky <20dika16@gmail.com>
2024-03-03 17:48:32 +0700

    09_ArrayOfObjects_ModifikasiPertanyaan4_Percobaan2

--- Minggu3/arrayObjects09.java
+++ Minggu3/arrayObjects09.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class arrayObjects09 {
     public static void main(String[] args) {
-        persegiPanjang[] ppArray09 = new persegiPanjang[3];
-
         Scanner sc09 = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
+        System.out.println("Masukkan jumlah Persegi Panjang: ");
+        int jml = sc09.nextInt();
+
+        persegiPanjang[] ppArray09 = new persegiPanjang[jml]; 
+
+        for (int i = 0; i < jml; i++) {
             ppArray09[i] = new persegiPanjang();
             System.out.println("Persegi Panjang ke- "+i);
             System.out.print("Masukkan Panjang: ");
@@ -17,7 +20,7 @@ public class arrayObjects09 {
             ppArray09[i].lebar = sc09.nextInt();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jml; i++) { 
             System.out.println("Persegi panjang ke- "+i);
             System.out.println("Panjang : "+ ppArray09[i].panjang + ", Lebar : " + ppArray09[i].lebar);
         }

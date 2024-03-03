f681bcecfc6c2c0d5f787898b576ab06f1eb8721
DikaArieArrifky <20dika16@gmail.com>
2024-03-03 18:47:10 +0700

    09_Percobaan3_Pertanyaan2

---
+++ Minggu3/ArraySegitiga/Segitiga.java
@@ -0,0 +1,11 @@
+package Minggu3.ArraySegitiga;
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga(int a, int t) {
+        alas = tinggi;
+        tinggi = t;
+    }
+}

1549b26ff2aa8b0821671771d6a35f9227789114
DikaArieArrifky <20dika16@gmail.com>
2024-03-04 20:40:30 +0700

    09_LatihanPraktikum_pertanyaan2

---
+++ Minggu3/programMahasiswa/mahasiswa09.java
@@ -0,0 +1,23 @@
+package Minggu3.programMahasiswa;
+
+public class mahasiswa09 {
+     String namaMHS;
+     String genderMHS;
+     double ipkMHS;
+     long nimMHS;
+    public String nama;
+    public long nim;
+    public String jenisKelamin;
+     
+    
+    
+    
+
+    public void dataMahasiswa(String nama, String gender, double ipk, long nim ){
+        namaMHS = nama;
+        genderMHS = gender;
+        ipkMHS = ipk;
+        nimMHS = nim;
+    }
+}
+

0e56dac49388b2073ef1207b4979e30896e9ab58
DikaArieArrifky <20dika16@gmail.com>
2024-03-04 23:21:12 +0700

    09_LatihanPraktikum_Pertanyaan2

--- Minggu3/programMahasiswa/MainMahasiswa09.java
+++ Minggu3/programMahasiswa/MainMahasiswa09.java
@@ -14,24 +14,24 @@ public class MainMahasiswa09 {
         for (int i = 0; i < 3; i++) {
             System.out.print("Masukkan data mahasiswa ke "+ (i+1));
             System.out.println();
-            mahasiswa09 mhs = new mahasiswa09();
+            
+            MhsArray[i] = new mahasiswa09();
             
             System.out.print("Masukkan nama : ");
-            mhs.nama = sc09.nextLine();
+            MhsArray[i].nama=sc09.nextLine();
             System.out.print("Masukkan nim : ");
-            mhs.nim = sc09.nextLong();
+            MhsArray[i].nim = sc09.nextLong();
             sc09.nextLine();
             System.out.print("Masukkan jenis kelamin : ");
-            mhs.jenisKelamin = sc09.nextLine();
+            MhsArray[i].jenisKelamin = sc09.nextLine();
             
             System.out.print("Masukkan IPK : ");
-            mhs.ipkMHS = Double.parseDouble(sc09.nextLine());
+            MhsArray[i].ipkMHS = Double.parseDouble(sc09.nextLine());
             System.out.println();
-
-            MhsArray[i]=mhs;
     
         } 
         for (int i  = 0; i < 3; i++) {
+            
             System.out.println("===Data Mahasiswa===");
             System.out.println("Data Mahasiswa ke- "+  (i+1));
             System.out.println("nama : "+ MhsArray[i].nama);

7f20902becf11bbe76f029b86d90fd2aed223644
DikaArieArrifky <20dika16@gmail.com>
2024-03-26 22:20:48 +0700

    09_Percobaan3_ModifikasiPertanyaan_Sorting

--- Minggu6/BubbleSelectionInsertion/src/Jobsheet6/DaftarMahasiswaBerprestasi09.java
+++ Minggu6/BubbleSelectionInsertion/src/Jobsheet6/DaftarMahasiswaBerprestasi09.java
@@ -49,7 +49,7 @@ public class DaftarMahasiswaBerprestasi09 {
         for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa09 temp = listMhs[i];
             int j = i-1;
-            while (j >= 0 && listMhs[j].ipk > temp.ipk) {
+            while (j >= 0 && listMhs[j].ipk < temp.ipk) {
                 listMhs[j+1] = listMhs[j];
                 j = j-1;
 

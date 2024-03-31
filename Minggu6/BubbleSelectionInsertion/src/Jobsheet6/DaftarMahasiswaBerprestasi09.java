0a25fb1e70687bc625b79aad4cb97e1833f668f1
DikaArieArrifky <20dika16@gmail.com>
2024-03-31 14:39:33 +0700

    09_Percobaan1 _Sorting

--- Minggu6/BubbleSelectionInsertion/src/Jobsheet6/DaftarMahasiswaBerprestasi09.java
+++ Minggu6/BubbleSelectionInsertion/src/Jobsheet6/DaftarMahasiswaBerprestasi09.java
@@ -21,11 +21,11 @@ public class DaftarMahasiswaBerprestasi09 {
 
     void bubbleShort(){
         for (int i = 0; i < listMhs.length-1; i++) {
-            for (int j = 0; j < listMhs.length-i-1; j++) {
-                if (listMhs[j].ipk < listMhs[j+1].ipk) {
+            for (int j = 1; j < listMhs.length-i; j++) {
+                if (listMhs[j].ipk > listMhs[j-1].ipk) {
                     Mahasiswa09 tmp = listMhs[j];
-                    listMhs[j] = listMhs[j+1];
-                    listMhs[j+1] = tmp;
+                    listMhs[j] = listMhs[j-1];
+                    listMhs[j-1] = tmp;
                 }
             }
         }

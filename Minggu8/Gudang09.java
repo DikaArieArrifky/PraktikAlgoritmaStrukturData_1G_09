0bb1472e98b5f98a79b50aaf05e5eb8625c58d8f
DikaArieArrifky <20dika16@gmail.com>
2024-04-29 23:05:01 +0700

    09_Percobaan1_ModifikasiPertanyaan4_Stack

--- Minggu8/Gudang09.java
+++ Minggu8/Gudang09.java
@@ -73,4 +73,6 @@ public class Gudang09 {
             System.out.println("Tumpukan barang kosong");
         }
     }
+
+    
 }
--- Minggu8/Utama09.java
+++ Minggu8/Utama09.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class Utama09 {
     public static void main(String[] args) {
-        Gudang09 gudang = new  Gudang09(7);
         Scanner sc09 = new Scanner(System.in);
+        System.out.println("Masukkan kapasitas gudang: ");
+        int kapasitas = sc09.nextInt();
+        Gudang09 gudang = new  Gudang09(kapasitas);
+        
 
         while (true) {
             System.out.println("\nMenu");
             System.out.println("1. Tambah barang");
             System.out.println("2. Ambil barang");
             System.out.println("3. Tampilkan tumpukan barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Tampilkan barang teratas");
+            System.out.println("5. Keluar");
             System.out.print("Pilih Operasi: ");
             int pilihan = sc09.nextInt();
             sc09.nextLine();
@@ -36,11 +40,15 @@ public class Utama09 {
                 gudang.ambilBarang();
                 break;
                 
-                case  3:
+                case 3:
                 gudang.tampilkanBarang();
                 break;
 
                 case 4:
+                gudang.lihatBarangTeratas09();
+                break;
+
+                case 5:
                 break;
 
                 default:

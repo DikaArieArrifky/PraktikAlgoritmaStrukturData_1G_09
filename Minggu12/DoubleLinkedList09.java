8753058c9f256c931c899611d73ebcf439682507
DikaArieArrifky <20dika16@gmail.com>
2024-05-14 10:07:17 +0700

    09_Percobaan1_DoubleLinkedList

---
+++ Minggu12/DoubleLinkedList09.java
@@ -0,0 +1,95 @@
+package Minggu12;
+
+public class DoubleLinkedList09 {
+    Node09 head;
+    int size;
+
+    public DoubleLinkedList09(){
+        head = null;
+        size = 0;
+
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void addFirst(int item){
+        if (isEmpty()) {
+            head = new Node09(null, item, null);           
+        } else {
+            Node09 newNode09 = new Node09(null, item, head);
+            head.prev = newNode09;
+            head = newNode09;
+        }
+        size++;
+
+    }
+
+    public void addLast(int item){
+        if (isEmpty()) {
+            addFirst(item);
+        } else {
+            Node09 current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            Node09 newNode09 = new Node09(current, item, null);
+            current.next = newNode09;
+            size++;
+
+        }
+    }
+
+    public void add(int item, int index) throws Exception{
+        if (isEmpty()) {
+            addFirst(item);
+        }else if (index < 0 || index > size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else {
+            Node09 current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.prev == null) {
+                Node09 newNode09 = new Node09(null, item, current);
+                current.prev = newNode09;
+                head = newNode09;
+            } else {
+                Node09 newNode09 = new Node09(current.prev, item, current);
+                newNode09.prev = current.prev;
+                newNode09.next = current;
+                current.prev.next = newNode09;
+                current.prev = newNode09;
+            }
+        }
+        size++;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void clear(){
+        head = null;
+        size = 0;
+        
+    }
+
+    public void print(){
+        if (!isEmpty()) {
+            Node09 tmp = head;
+            while (tmp != null) {
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }  
+            System.out.println("\nberhasil diisi");
+        } else {
+            System.out.println("Linked list kosong");
+        }
+    }
+
+    
+}
---
+++ Minggu12/Node09.java
@@ -0,0 +1,13 @@
+package Minggu12;
+
+public class Node09 {
+    /**...4 lines */
+        int data;
+        Node09 next,prev;
+
+        Node09(Node09 prev, int data, Node09 next){
+            this.prev = prev;
+            this.data = data;
+            this.next = next;
+        }
+}

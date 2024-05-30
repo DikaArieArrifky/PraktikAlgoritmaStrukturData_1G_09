fb6cb225347884afcfff9f2b69d02f3381da53b9
DikaArieArrifky <20dika16@gmail.com>
2024-05-30 20:29:02 +0700

    09_Percobaan1_tree

---
+++ Minggu13/Node09.java
@@ -0,0 +1,17 @@
+package Minggu13;
+
+public class Node09 {
+    int data;
+    Node09 left;
+    Node09 right;
+
+    public Node09(){
+   
+    }
+
+    public Node09(int data){
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}   
